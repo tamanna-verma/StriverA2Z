fafb01ea5f8e638ca35a78a921d4ea88d218f9f9
Tamanna Verma <tamannav01@gmail.com>
2024-02-05 00:08:21 +0530

    Create 11.FindOutHowManyTimesIsArrayRotated.java

---
+++ 04.BinarySearch/11.FindOutHowManyTimesIsArrayRotated.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public static int findKRotation(int []arr){
+           
+          int low = 0;
+          int index=-1;
+         int high = arr.length-1;
+         int ans=Integer.MAX_VALUE;
+         while(low<=high)
+         {
+             int mid  = low+ ((high-low)/2);
+             
+             //if left half is sorted 
+       
+              
+             if(arr[low]<=arr[mid])
+               {
+                  if(arr[low]<ans)
+                  {
+                   ans=arr[low];
+                   index=low;
+                  }
+                  low = mid+1;
+               }
+              //if right half is sorted
+               else 
+               {
+                  if(arr[mid]<ans)
+                  {
+                     ans=arr[mid];
+                     index=mid;
+                  }
+                 high = mid-1;
+               }
+            
+         }
+         return index;
+
+
+}
+}
