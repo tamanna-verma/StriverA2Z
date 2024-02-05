b26aa51e59c2ce55bf7db68a4210f518e044cecc
Tamanna Verma <tamannav01@gmail.com>
2024-02-05 15:38:10 +0530

    Update 13.FindPeakElement.java

--- 04.BinarySearch/13.FindPeakElement.java
+++ 04.BinarySearch/13.FindPeakElement.java
@@ -6,9 +6,9 @@ class Solution {
   
     public int findPeakElement(int[] nums) {
         int n=nums.length;
-        if(n==1)return nums[0];
-        if(nums[0]>nums[1])return nums[0];
-        if(nums[n-1]>nums[n-2])return nums[n-1];
+        if(n==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[n-1]>nums[n-2])return n-1;
         int low=1;
         int high=n-2;
         while(low<=high)
