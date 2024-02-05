d9ac5ce2fe2776b5af122ecb217af60bed26fb96
Tamanna Verma <tamannav01@gmail.com>
2024-02-05 15:37:05 +0530

    Create 13.FindPeakElement.java

---
+++ 04.BinarySearch/13.FindPeakElement.java
@@ -0,0 +1,25 @@
+class Solution {
+
+  //if element is greater than left and right , it meaning it is peak
+ // if mid greater than left , it means it is in increasing curve and move low to mid +1 and search in right side
+// if mid is lesser than right , it means it is in decreasing curve , serach in left side and make high=mid-1
+  
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if(n==1)return nums[0];
+        if(nums[0]>nums[1])return nums[0];
+        if(nums[n-1]>nums[n-2])return nums[n-1];
+        int low=1;
+        int high=n-2;
+        while(low<=high)
+        {
+            int mid = low+ ((high-low)/2);//mid ki value milgyi hai 
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return mid;
+            if(nums[mid]<nums[mid+1])low=mid+1;
+            else  if(nums[mid]<nums[mid-1]) high=mid-1;
+        }
+
+return -1;
+    }
+}
+//here peak can be more than 1 
