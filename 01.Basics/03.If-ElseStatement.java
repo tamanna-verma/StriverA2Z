113689080afc3448bf7e0ccf1aa8a8a9f63d83dc
Tamanna Verma <tamannav01@gmail.com>
2024-04-06 13:21:31 +0530

    Update 03.If-ElseStatement.java

--- 01.Basics/03.If-ElseStatement.java
+++ 01.Basics/03.If-ElseStatement.java
@@ -1,9 +1,19 @@
+//way 1
 public class Solution {
     public static String compareIfElse(int a, int b) {
      
      if(a>b)return("greater");
      else if(a<b)return("smaller");
      else return("equal");
+      
+    }
+}
+
+//WAY 2
+public class Solution {
+    public static String compareIfElse(int a, int b) {
+     
+return (a>b)?"greater":(a<b)?"smaller":"equal";
       
     }
 }
